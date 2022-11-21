6db236b865b64f3a4384cd7a87a230a0d40f94e7
Lucas Low <53358104+lucas-low@users.noreply.github.com>
2022-11-21 15:03:36 +0800

    added esbuild react typescript

---
+++ app/javascript/components/application.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+// interface arg to check valid TS syntax 
+interface AppProps {
+  arg: string;
+}
+
+const App = ({ arg }: AppProps) => {
+  return <div>{`Hello, ${arg}!`}</div>;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rootEl = document.getElementById("root");
+  ReactDOM.render(<App arg="Rails 7 with ESBuild" />, rootEl);
+});
